de6e756819a523109c630c98d159ecc56e926f9a
shivang <shivangmathur101@gmail.com>
2022-03-15 19:23:31 +0530

    inital files and images

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button>New Game</button>
+    </div>
+  );
+}
+
+export default App;
